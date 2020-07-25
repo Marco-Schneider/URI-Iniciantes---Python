cd94e384d5db96ae10723fd7edc8c1a055c86059
Marco-Schneider <58363718+Marco-Schneider@users.noreply.github.com>
2020-07-25 17:46:44 -0300

    Accepted

--- 1008.py
+++ 1008.py
@@ -1,1 +1,8 @@
 #URI Online Judge - Iniciantes 1008
+
+numEmpregado = int(input()) #Número que identifica o empregado
+horasTrabalhadas = int(input()) #Número de horas trabalhadas
+qntPorHora = float(input()) #Valor recebido por hora trabalhada
+
+print("NUMBER =", numEmpregado)
+print("SALARY = U$", "%.2f" % float(horasTrabalhadas*qntPorHora))
